457bf05cfbdac616bb08edc575e9f4c4ee08d2b6
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-08-14 11:56:20 +0900

    Enhance 用户权限管理系统

---
+++ user_permission_management_0814_1056_bjt.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-08-14 10:56:18
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit, QVBoxLayout, QWidget, QPushButton, QComboBox, QGridLayout, QLabel, QFormLayout, QLineEdit, QMessageBox
+from PyQt5.QtCore import Qt
+
+"
+# NOTE: 重要实现细节
