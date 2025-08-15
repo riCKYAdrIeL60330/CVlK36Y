2611cd616dfbb957036bf26f02a928211f3ab728
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-08-16 02:14:50 +0900

    Enhance RESTful API接口开发

---
+++ restful_api_with_pyqt_0816_0114_gin.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-08-16 01:14:47
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtNetwork import QLocalSocket, QLocalServer, QAbstractSocket
+
+"
