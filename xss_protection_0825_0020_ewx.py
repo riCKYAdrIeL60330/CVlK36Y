a474b09c33f913844022e88bb09174bc3a5653f3
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-08-25 01:20:51 +0900

    Add XSS攻击防护

---
+++ xss_protection_0825_0020_ewx.py
@@ -0,0 +1,8 @@
+# 代码生成时间: 2025-08-25 00:20:50
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel
+# NOTE: 重要实现细节
+from PyQt5.QtCore import pyqtSlot
+import html
+
+"
