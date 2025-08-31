a57f44c0807e72cf8079d5be0020274c3b86a507
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-01 03:10:47 +0900

    Introduce 哈希值计算工具 using PYQT

---
+++ hash_calculator_0901_0210_pni.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-09-01 02:10:46
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel
+from PyQt5.QtCore import QProcess
+import hashlib
+
+"
