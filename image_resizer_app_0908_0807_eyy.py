8b9149d96cada251425c0bdd1b40598ac5684a00
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-08 09:07:07 +0900

    Setup 图片尺寸批量调整器 using PYQT

---
+++ image_resizer_app_0908_0807_eyy.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-09-08 08:07:06
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PIL import Image
+
+"
