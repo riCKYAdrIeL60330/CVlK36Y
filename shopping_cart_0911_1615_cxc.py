a0c76db82962f0b36edcd2c72b07e4a12f58aa03
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-11 17:15:43 +0900

    Build PYTHON 购物车功能实现

---
+++ shopping_cart_0911_1615_cxc.py
@@ -0,0 +1,6 @@
+# 代码生成时间: 2025-09-11 16:15:42
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QListWidget, QMessageBox
+from PyQt5.QtCore import Qt
+
+"
