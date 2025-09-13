eccdba61d20eb2c458da7faaa64a91567588141c
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-13 19:47:18 +0900

    Configure new 网页内容抓取工具 feature

---
+++ web_scraper_0913_1847_zdf.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-09-13 18:47:17
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QTextEdit, QWidget
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineCore import QWebEngineProfile
+
+"
