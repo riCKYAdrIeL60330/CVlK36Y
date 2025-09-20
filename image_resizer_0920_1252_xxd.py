00bc7589a7cce5b92d63a60ed605b5de8f21d65e
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-20 13:52:38 +0900

    Enhance PYTHON 图片尺寸批量调整器

---
+++ image_resizer_0920_1252_xxd.py
@@ -0,0 +1,10 @@
+# 代码生成时间: 2025-09-20 12:52:36
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel, QSlider, QSpinBox, QComboBox, QCheckBox
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import os
+from PIL import Image
+# TODO: 优化性能
+
+"
