533bd717d124ea9170501de097f594031857b07e
riCKYAdrIeL60330 <rarity_con0p@icloud.com>
2025-09-20 20:41:08 +0900

    Setup new 访问权限控制 feature

---
+++ access_control_0920_1941_lnh.py
@@ -0,0 +1,6 @@
+# 代码生成时间: 2025-09-20 19:41:06
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+from PyQt5.QtCore import Qt
+
+"
